99955946b202295d61d12467b2c92e422935a98c
Raim Interiano <rai.interiano@gmail.com>
2016-11-16 15:33:29 -0600

    Double mutex for correct ping-pong

--- Sema/ping_pong.c
+++ Sema/ping_pong.c
@@ -6,55 +6,59 @@
 #define NITER 5
 
 int cnt = 0;
-sem_t mutex;
+sem_t ping_mutex, pong_mutex;
 
-void* ping_pong(void* arg){
+void* ping(void* arg){
   int i = 0;
   int value = *(int*)arg;
   for(i = 0; i < NITER; i++)
   {
-    usleep(1);
-    sem_wait(&mutex);
-    int val;
-    sem_getvalue(&mutex, &val);
-    //printf("Val sem_wait: %d\n", val);
-    if(cnt++ % 2 == 0)
-      printf("Ping! from %d\n", value);
-    else
-      printf("Pong! from %d\n", value);
-    sem_post(&mutex);
-    sem_getvalue(&mutex, &val);
-    //printf("Val sem_post: %d\n", val);
+    sem_wait(&pong_mutex);
+    printf("Ping!\n");
+    sem_post(&ping_mutex);
   }
 }
+
+void* pong(void* arg){
+  int i = 0;
+  int value = *(int*)arg;
+  for(i = 0; i < NITER; i++)
+  {
+    sem_wait(&ping_mutex);
+    printf("Pong!\n");
+    sem_post(&pong_mutex);
+  }
+}
+
 
 int main(int argc, char * argv[])
 {
 
-    sem_init(&mutex, 0, 1);
+    sem_init(&ping_mutex, 0, 1);
+    sem_init(&pong_mutex, 0, 1);
 
-    pthread_t ping, pong;
+    pthread_t ping_thread, pong_thread;
     int value = 5;
     int val2 = 10;
-    if(pthread_create(&ping, NULL, ping_pong, (void*)&value))
+    if(pthread_create(&ping, NULL, ping_thread, (void*)&value))
     {
       printf("\n ERROR creating thread ping");
       exit(1);
     }
 
-    if(pthread_create(&pong, NULL, ping_pong, (void*)&val2))
+    if(pthread_create(&pong, NULL, pong_thread, (void*)&val2))
     {
       printf("\n ERROR creating thread pong");
       exit(1);
     }
 
-    if(pthread_join(ping, NULL))
+    if(pthread_join(ping_thread, NULL))
     {
       printf("\n ERROR joining thread");
       exit(1);
     }
 
-    if(pthread_join(pong, NULL))
+    if(pthread_join(pong_thread, NULL))
     {
       printf("\n ERROR joining thread");
       exit(1);
@@ -63,7 +67,8 @@ int main(int argc, char * argv[])
     printf("Cnt is: %d\n", cnt);
     pthread_exit(NULL);
 
-    sem_destroy(&mutex);
+    sem_destroy(&ping_mutex);
+    sem_destroy(&pong_mutex);
 
     return 0;
 }
