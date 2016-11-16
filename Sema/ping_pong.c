6ec6d6c9d84efd171687bbd08706c2a6b915edf2
Raim Interiano <rai.interiano@gmail.com>
2016-11-16 13:13:37 -0600

    Sema testing with usleep

--- Sema/ping_pong.c
+++ Sema/ping_pong.c
@@ -10,14 +10,21 @@ sem_t mutex;
 
 void* ping_pong(void* arg){
   int i = 0;
+  int value = *(int*)arg;
   for(i = 0; i < NITER; i++)
   {
+    usleep(1);
     sem_wait(&mutex);
+    int val;
+    sem_getvalue(&mutex, &val);
+    //printf("Val sem_wait: %d\n", val);
     if(cnt++ % 2 == 0)
-      printf("Ping!\n");
+      printf("Ping! from %d\n", value);
     else
-      printf("Pong!\n");
+      printf("Pong! from %d\n", value);
     sem_post(&mutex);
+    sem_getvalue(&mutex, &val);
+    //printf("Val sem_post: %d\n", val);
   }
 }
 
@@ -27,14 +34,15 @@ int main(int argc, char * argv[])
     sem_init(&mutex, 0, 1);
 
     pthread_t ping, pong;
-
-    if(pthread_create(&ping, NULL, ping_pong, NULL))
+    int value = 5;
+    int val2 = 10;
+    if(pthread_create(&ping, NULL, ping_pong, (void*)&value))
     {
       printf("\n ERROR creating thread ping");
       exit(1);
     }
 
-    if(pthread_create(&pong, NULL, ping_pong, NULL))
+    if(pthread_create(&pong, NULL, ping_pong, (void*)&val2))
     {
       printf("\n ERROR creating thread pong");
       exit(1);
@@ -54,4 +62,8 @@ int main(int argc, char * argv[])
 
     printf("Cnt is: %d\n", cnt);
     pthread_exit(NULL);
+
+    sem_destroy(&mutex);
+
+    return 0;
 }
